ed75b0f5054c01d9863ca6af782794d35d5a48fa
abdou <abdou@glmail.coom>
2025-02-28 09:29:55 +0000

    a

--- push_swap.c
+++ push_swap.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-hadj <ael-hadj@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 09:21:35 by ael-hadj          #+#    #+#             */
+/*   Updated: 2025/02/28 09:21:35 by ael-hadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include "libft/libft.h"
 
 char	**return_string(int ac, char **av)
 {
-	int	i;
-	char	*numbers;
-	char	*temp;
-	char	**arr;
+	char		*numbers;
+	char		*temp;
+	char		**arr;
+	int			i;
 
 	numbers = ft_strdup(av[1]);
 	i = 2;
