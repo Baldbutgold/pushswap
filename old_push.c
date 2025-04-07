7ed74b1324ead901223a37bf39128f033b19e9a2
abdou <abdou@glmail.coom>
2025-04-07 10:49:00 +0100

    working on push

--- old_push.c
+++ old_push.c
@@ -12,20 +12,40 @@
 
 #include "push_swap.h"
 #include "libft/libft.h"
-#include <stdio.h>
+
+char	**return_string(int ac, char **av)
+{
+	char		*numbers;
+	char		*temp;
+	char		**arr;
+	int			i;
+
+	numbers = ft_strdup(av[1]);
+	i = 2;
+	while (i < ac)
+	{
+		temp = numbers;
+		numbers = ft_strjoin(temp, " ");
+		free(temp);
+		temp = numbers;
+		numbers = ft_strjoin(temp, av[i++]);
+		free(temp);
+	}
+	arr = ft_split(numbers, ' ');
+	free(numbers);
+	return (arr);
+}
 
 int	main(int ac, char **av)
 {
+	char **arr;
+
 	if (ac > 2)
 	{
-		int	i;
-
-		i = 1;
-		while (av[i])
-		{
-			if (av[i][0] == '\0')
-				return (ft_putstr_fd("Error\n", 2), 0);
-			i++;
-		}
+		arr = return_string(ac, av);
+		int i = 0;
+		while (arr[i])
+			ft_printf("%s",arr[i++]);
+		free_grid(arr);
 	}
 }
